d627a1110a1076b0009c6f65c7f69dc928dbaa64
jwinkler <jan.winkler@markt.de>
2021-12-08 18:10:39 +0100

    update to version 3.3.3

--- openvpn-connect/tools/chocolateyinstall.ps1
+++ openvpn-connect/tools/chocolateyinstall.ps1
@@ -5,16 +5,27 @@ $packageArgs = @{
   packageName     = 'openvpn-connect'
   fileType        = 'MSI'
   softwareName    = 'OpenVPN-Connect'
-  url             = 'https://swupdate.openvpn.net/downloads/connect/openvpn-connect-3.2.3.1851_signed_x86.msi'
-  checksum        = 'bfd176271cca8c1e7fae446621a985388bf55d023668af8e5dcec9ec4560dd85'
+  url             = 'https://swupdate.openvpn.net/downloads/connect/openvpn-connect-3.3.3.2562_signed_x86.msi'
+  checksum        = 'bb8447a4776a5ddee887250274b9e0dec730a5ef1dcef6b284c18829e02ddf7e'
   checksumType    = 'sha256'
-  url64bit        = 'https://swupdate.openvpn.net/downloads/connect/openvpn-connect-3.2.3.1851_signed.msi'
-  checksum64      = '58973ceba1dfb77ac98977f264b5aebe4ca094c17ada72c4d2767d2a2c7607c5'
+  url64bit        = 'https://swupdate.openvpn.net/downloads/connect/openvpn-connect-3.3.3.2562_signed.msi'
+  checksum64      = 'bb8447a4776a5ddee887250274b9e0dec730a5ef1dcef6b284c18829e02ddf7e'
   checksumType64  = 'sha256'
   silentArgs      = "/qn"
   validExitCodes  = @(0, 3010, 1641)
 }
 
-Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
+Get-ChocolateyWebFile -packageName $packageArgs['packageName'] `
+-fileFullPath "${toolsDir}\openvpn-connect.msi" `
+-Url $packageArgs['url64bit'] `
+-checksum $packageArgs['checksum64'] `
+-checksumType $packageArgs['checksumType64'] 
+$msiFile = Join-Path $toolsDir 'openvpn-connect.msi'
+$outputFile = Join-Path $toolsDir 'openvpn-cert.cer'
+$exportType =[System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
+$cert = (Get-AuthenticodeSignature $msiFile).SignerCertificate
+[System.IO.File]::WriteAllBytes($outputFile, $cert.Export($exportType))
+certutil -addstore -f "TrustedPublisher" $toolsdir\openvpn-cert.cer
+
 # Start installation process
 Install-ChocolateyPackage @packageArgs
