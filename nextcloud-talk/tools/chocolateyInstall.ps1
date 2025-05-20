7877d48f484d30c0c6026e4d31cd3282b950d2c3
Chocolatey <chocolatey@realdimensions.net>
2025-05-20 10:01:14 +0000

    AU: 2 updated - nextcloud-talk openvpn-connect [skip ci] https://gist.github.com/jan-win1993/e7559af0f2141b4a69d06949c168ff7d/e424a55e45aa499069cfe548ff779ac67141b16c

--- nextcloud-talk/tools/chocolateyInstall.ps1
+++ nextcloud-talk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   
   fileType               = 'msi'
  
-  url64                  = 'https://github.com/nextcloud-releases/talk-desktop/releases/download/v1.1.8/Nextcloud.Talk-windows-x64.msi'
-  checksum64             = 'e3520934a80b6026c1541cc94926cb33bc2547581ac5cab603db34e71af46916'
+  url64                  = 'https://github.com/nextcloud-releases/talk-desktop/releases/download/v1.1.9/Nextcloud.Talk-windows-x64.msi'
+  checksum64             = '9395100f61705fc7ee58b56dc5dcbd8e32ff50f0ac96e02b123f0d6ee12edf6f'
   checksumType64         = 'sha256'
   silentArgs             = '/quiet /norestart INSTALLLEVEL=1'
   validExitCodes         = @(0)
