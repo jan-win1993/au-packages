8e001da141ff2e0607e06da54587912eee62a53c
Chocolatey <chocolatey@realdimensions.net>
2025-10-10 10:01:07 +0000

    AU: 1 updated - nextcloud-talk [skip ci] https://gist.github.com/jan-win1993/e7559af0f2141b4a69d06949c168ff7d/071f115d1989c923ba22eb03adcf46454d033819

--- nextcloud-talk/tools/chocolateyInstall.ps1
+++ nextcloud-talk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   
   fileType               = 'msi'
  
-  url64                  = 'https://github.com/nextcloud-releases/talk-desktop/releases/download/v1.2.6/Nextcloud.Talk-windows-x64.msi'
-  checksum64             = '1172f89f0f0ad19c97d881e94e6540e039c70f758f81c4b163cfc04091c34e73'
+  url64                  = 'https://github.com/nextcloud-releases/talk-desktop/releases/download/v2.0.2/Nextcloud.Talk-windows-x64.msi'
+  checksum64             = '8538a1617ffc68852121af7361f624fd60ef3985429548269a90d58cd3d7b6d5'
   checksumType64         = 'sha256'
   silentArgs             = '/quiet /norestart INSTALLLEVEL=1'
   validExitCodes         = @(0)
